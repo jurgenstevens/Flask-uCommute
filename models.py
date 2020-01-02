425efd70129c8b93da0634b3ae3f7cfb4b4257c8
Jurgen <jurgen.stevens@yahoo.com>
2020-01-01 22:42:54 -0600

    Fixed indentationin models.py on conditional

--- models.py
+++ models.py
@@ -11,12 +11,13 @@ if 'ON_HEROKU' in os.environ:
 # later we will manually add this env var 
                              
 # in heroku so we can write this code
-DATABASE = connect(os.environ.get('DATABASE_URL')) 
+	DATABASE = connect(os.environ.get('DATABASE_URL')) 
 # heroku will add this 
 # env var for you 
 # when you provision the
 # Heroku Postgres Add-on
 else:
+	DATABASE = SqliteDatabase('ucommute.sqlite') 
   # OPTIONALLY: instead of the above line, here's how you could have your 
   # local app use PSQL instead of SQLite:
 
@@ -25,8 +26,8 @@ else:
   # the first argument is the database name -- YOU MUST MANUALLY CREATE 
   # IT IN YOUR psql TERMINAL
   # the second argument is your Unix/Linux username on your computer
-  
-	DATABASE = SqliteDatabase('ucommute.sqlite') # you want to give it the name .sqlite extension
+
+# you want to give it the name .sqlite extension
 # when we want to go to production we'll use our postgres
 
 # to be have correctly in flask-login's session/login in functionality, the User class
